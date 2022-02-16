fb2ae2c5e3d521bbfd2d69c4935ab0fa63691427
Joff-prch <joffrey.prch@gmail.com>
2022-02-16 21:52:06 +0000

    Finalisation

--- assets/main.js
+++ assets/main.js
@@ -1,20 +1,3 @@
-// let btnAddPokemon = document.querySelector('.add_pokemon');
-// let btnFormPokemon = document.querySelector('.form_title button');
-// let form = document.querySelector('.main_form');
-// let mainContent = document.querySelector('.main_content');
-
-
-// btnAddPokemon.addEventListener('click', ()=>{
-//     form.classList += ' appear';
-//     mainContent.classList += ' blur_body';
-// })
-
-// btnFormPokemon.addEventListener('click', ()=>{
-//     form.classList = 'main_form';
-//     mainContent.classList = 'main_content';
-
-
-// })
 
 
 function disco(){
